c30c7d65b414c16dfaf0c56917abf7fbd44cc774
cal006699 <calvin.vu26@gmail.com>
2026-04-22 21:05:36 +0200

    First commit

---
+++ Multiburo/database.inc.php
@@ -0,0 +1,9 @@
+<?php
+define('SGBDR', 'mysql');
+define('HOST', 'localhost');
+define('PORT', '3306');
+define('LOGIN', 'calvin_vu');
+define('PASSW', 'bonjoure');
+define('BDD', 'multiburo_calvin');
+
+?>
